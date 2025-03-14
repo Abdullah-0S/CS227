5839c6626a1407fe1b08fcc971a7445ce9ff0c47
MohammedShmhani <158126523+MohammedShmhani@users.noreply.github.com>
2025-03-14 01:10:01 +0000

    Updated FCFS scheduling with time tracking

--- FCFS.java
+++ FCFS.java
@@ -1,16 +1,53 @@
 import java.util.Queue;
 
 public class FCFS {
-    long time; // This is waiting time in queue set for each process
+    private model systemModel;
+    private long time = 0; // يمثل وقت الانتظار في الطابور لكل عملية
 
-    // I Want u to implement the following algorithim and when process is started set its waiting time and finish time 
-    public Queue<PCB> FCFSS(Queue<PCB> RunningQueue) {
-      
-        return null;
+    public FCFS(model systemModel) {
+        this.systemModel = systemModel;
     }
 
-    // Testing
+    public void schedule() {
+        Queue<PCB> readyQueue = systemModel.readyQueue;
+        
+        System.out.println("\nExecuting FCFS Scheduling:");
+
+        while (!readyQueue.isEmpty()) {
+            PCB process = readyQueue.poll();
+            
+            // حساب زمن الانتظار لكل عملية
+            process.setTime(time);
+            process.setState(State.RUNNING);
+            process.setFirstResponseTime((int) time);
+
+            System.out.println("Process " + process.getId() + " executed from " + time + " to " + (time + process.getBurstTime()));
+
+            // تحديث الزمن
+            time += process.getBurstTime();
+            
+            process.setState(State.Terminated);
+            systemModel.free(process);
+        }
+    }
+    
+    // Testing the FCFS Scheduler
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        model systemModel = new model();
+
+        // Read processes from file and load into the system
+        for (int[] processInfo : readFile.read("job.txt")) {
+            PCB process = new PCB(processInfo);
+            systemModel.createProcess(process);
+        }
+
+        // Move jobs to ready queue
+        while (!systemModel.JobQueue.isEmpty()) {
+            systemModel.load();
+        }
+
+        // Run FCFS Scheduler
+        FCFS fcfsScheduler = new FCFS(systemModel);
+        fcfsScheduler.schedule();
     }
 }
